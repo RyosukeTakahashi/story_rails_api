6e12e8e719114f245e1bfaf02f031c9c27ff060d
Ryo Murakami <ryo.a.murakami@dena.com>
2019-05-23 18:08:21 +0900

    set routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'api/stories', to: 'stories#index'
+  post 'api/stories', to: 'stories#create'
+
+  get 'api/stories/:story_id/pages/:page_id', to: 'pages#show_page_in_story'
+  post 'api/stories/:story_id/pages/:parent_id/next', to: 'pages#save_page_in_story'
+
 end
