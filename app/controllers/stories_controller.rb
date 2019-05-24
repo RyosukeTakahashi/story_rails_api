f8a9ff65637a85007dd9c91f4accc3b2a3e61467
Ryo Murakami <ryo.a.murakami@dena.com>
2019-05-24 12:08:37 +0900

    Add save method to Stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,5 +6,27 @@ class StoriesController < ApplicationController
     render json: stories_with_parent_id
   end
 
+  def create
+    story_params = {
+        title: params[:title],
+        summary: params[:summary],
+    }
+    @story = Story.new(story_params)
+
+    if @story.save
+      # render :show, status: :created, location: @story
+      render json: @story
+    else
+      render json: @story.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def story_params
+    params.require(:story).permit(:title, :summary)
+  end
+
+
 end
 
--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 require 'json'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
-  test "stories" do
-    get 'http://localhost:3001/api/stories'
+  test "stories show" do
+    get '/api/stories'
     result_hash = JSON.parse(response.body)
     assert (result_hash[0]["title"] == 'MyString')
     assert (result_hash[0]["parentId"] == nil)
   end
+
+  test "stories save" do
+    post "/api/stories",
+         params: { title: "new story title", summary: "new story text" }
+    assert_response :success
+
+  end
+
 end
