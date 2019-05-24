bc694922997852cd3697dbbf813ead8b201fae0a
Ryo Murakami <ryo.a.murakami@dena.com>
2019-05-24 13:48:29 +0900

    Add save method and test code.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,25 @@ class PagesController < ApplicationController
     render json: @page
   end
 
+  def save_page_in_story
+    page_params = {
+        name: params[:name],
+        text: params[:text],
+        story_id: params[:story_id],
+        parent_id: params[:parent_id]
+    }
+    @page = Page.new(page_params)
+    if @page.save
+      render json: @page
+    else
+      render json: @page.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def page_params
+    params.require(:page).permit(:name, :text, :story_id, :parent_id)
+  end
+
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'json'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "show story" do
+  test "show page" do
 
     page_id = Page.all[0].id
 
@@ -10,4 +10,16 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     result_hash = JSON.parse(response.body)
     assert (result_hash["name"] == "MyString")
   end
+
+  test "stories save" do
+    page_name = "new page name"
+    post "/api/stories/2/pages/3/next",
+         params: { name: page_name, text: "new page text", story_id: 1, parent_id: nil}
+    assert_response :success
+    page_id = JSON.parse(response.body)["id"]
+    get "http://localhost:3001/api/stories/2/pages/#{page_id}"
+    result_hash = JSON.parse(response.body)
+    assert (result_hash["name"] == page_name)
+  end
+
 end
