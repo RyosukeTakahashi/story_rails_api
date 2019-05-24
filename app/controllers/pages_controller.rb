73b0cf627d3ecc509e051287d6af6cfb3e324a20
Ryo Murakami <ryo.a.murakami@dena.com>
2019-05-24 11:46:26 +0900

    Add controller with a show method. Add controller test.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+
+  def show_page_in_story
+    @page = Page.where(story_id: params[:story_id]).where(id: params[:page_id])[0]
+    render json: @page
+  end
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'json'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "show story" do
+
+    page_id = Page.all[0].id
+
+    get "http://localhost:3001/api/stories/1/pages/#{page_id}"
+    result_hash = JSON.parse(response.body)
+    assert (result_hash["name"] == "MyString")
+  end
 end
